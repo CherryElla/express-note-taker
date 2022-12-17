ed6601a94d6d6e6ea066e5150d7bcda473f8f125
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2022-12-16 20:39:07 -0500

    created function to send notes to server

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -1,3 +1,20 @@
 const userNoteInput = document.getElementById("noteTitle")
 const userNoteText = document.getElementById("noteText")
 
+const sendNoteObj = (note) => {
+    fetch("/api/notes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(note),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log("Successful POST request:", data)
+        return data
+    })
+    .catch((error) => {
+        console.error("Error in POST request:", error)
+    })
+}
