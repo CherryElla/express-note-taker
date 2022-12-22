99c389989b4a2a060e2d1557b1c0784da88deeea
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2022-12-22 15:59:23 -0500

    finished up post request for saving note

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.static("public"))
 
 
-// Get requests
+// Get requests (listening for )
 app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "Develop", "public", "notes.html"))
 })
@@ -35,6 +35,8 @@ app.get("*",(req,res) => {
 
 app.post("/api/notes", (req, res) => {
     console.info(`${req.method} request received to add a note`)
+    notes.push(req.body)
+    res.sendStatus(200)
 })
 
 
